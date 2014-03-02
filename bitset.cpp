325a7149156994b954384000c3ada3ed6c758818
michel94 <miguel.duarte20@hotmail.com>
2014-03-02 00:41:38 +0000

    Added bitset and union-find implementations.

---
+++ bitset.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void printset(unsigned int set, int size){
+	int i;
+	for(i=0; i<size; i++){
+		printf("%d", set%2);
+		set = set >> 1;
+	}
+	putchar('\n');
+}
+
+int isset(unsigned int set, int p){
+	return (set >> p) & 0x1;
+}
+
+int setp(unsigned int set, int pos){
+	set = set | (0x1 << pos);
+	return set;
+}
+
+int unsetp(unsigned int set, int pos){
+	set = set ^ (0x1 << pos);
+}
---
+++ unionfind.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int parent(int v){
+	if(uf[v] == uf[uf[v]]){
+		return uf[v];
+	}
+	uf[v] = parent(uf[v]);
+	return uf[v];
+}
+
+void uni(int v1, int v2){
+	int p1 = parent(v1), p2 = parent(v2);
+	uf[p1] = p2;
+}
+
+inline int find(int v1, int v2){
+	return parent(v1) == parent(v2);
+}
